5127b10da5ba2cea902880177d1445a1b9efee48
wubh2012 <wubh2012@163.com>
2019-08-15 17:27:47 +0800

    指定按钮的类型，使用对应的样式，primary,success,info,warning,danger

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,48 @@
 <template>
   <div id="app">
-    <w-button>按钮1</w-button>
-    <w-button>按钮2</w-button>
-    <w-button>按钮3</w-button>
+    <div id="demo">
+      <w-button>默认按钮</w-button>
+      <w-button type="primary">主要按钮</w-button>
+      <w-button type="success">成功按钮</w-button>
+      <w-button type="info">信息按钮</w-button>
+      <w-button type="warning">警告按钮</w-button>
+      <w-button type="danger">危险按钮</w-button>
+    </div>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'app',
-  
-}
+  name: "app"
+};
 </script>
 
 <style lang="scss">
-*{
+* {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
 }
-:root{
+:root {
   --font-size: 14px;
   --color: #333;
 }
-body{
+body {
   font-size: var(--font-size);
 }
 #app {
-  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'PingFang SC', 'Hiragino Sans GB',
-  'Microsoft YaHei', 'Helvetica Neue', Helvetica, Arial, sans-serif, 'Apple Color Emoji',
-  'Segoe UI Emoji', 'Segoe UI Symbol';
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "PingFang SC",
+    "Hiragino Sans GB", "Microsoft YaHei", "Helvetica Neue", Helvetica, Arial,
+    sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#demo {
+  .w-button + .w-button {
+    margin-left: 10px;
+  }
 }
 </style>
--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,5 +1,7 @@
 <template>
-  <button class="w-button">
+  <button class="w-button" :class="[
+    type? 'w-button--'+type : ''
+  ]">
     <slot></slot>
   </button>
 </template>
@@ -7,6 +9,12 @@
 <script>
 export default {
   name: "w-button",
+  props: {
+    type: {
+      type: String,
+      default: "default"
+    }
+  },
   data() {
     return {};
   }
@@ -18,8 +26,7 @@ $button-height: 32px;
 $button-bg: white;
 $button-active-bg: #eee;
 $color: #333;
-$border-color: #999;
-$border-color-hover: #666;
+$border-color: #dcdfe6;
 $border-radius: 4px;
 .w-button {
   font-size: var(--font-size);
@@ -28,14 +35,99 @@ $border-radius: 4px;
   border: 1px solid $border-color;
   border-radius: $border-radius;
   background: $button-bg;
-  &:hover {
-    border-color: $border-color-hover;
+  outline: none;
+  transition: 0.1s;
+  &:hover,
+  &:focus {
+    color: #409eff;
+    border-color: #c6e2ff;
+    background-color: #ecf5ff;
   }
   &:active {
-    background-color: $button-active-bg;
-  }
-  &:focus {
+    color: #3a8ee6;
+    border-color: #3a8ee6;
     outline: none;
+  }
+
+  &--primary {
+    color: #fff;
+    background-color: #409eff;
+    border-color: #409eff;
+    &:hover,
+    &:focus {
+      color: #fff;
+      background: #66b1ff;
+      border-color: #66b1ff;
+    }
+    &:active {
+      color: #fff;
+      background: #3a8ee6;
+      border-color: #3a8ee6;
+    }
+  }
+  &--success {
+    color: #fff;
+    background-color: #67c23a;
+    border-color: #67c23a;
+    &:hover,
+    &:focus {
+      background: #85ce61;
+      border-color: #85ce61;
+      color: #fff;
+    }
+    &:active {
+      background: #5daf34;
+      border-color: #5daf34;
+      color: #fff;
+    }
+  }
+  &--info {
+    color: #fff;
+    background-color: #909399;
+    border-color: #909399;
+    &:hover,
+    &:focus {
+      background: #a6a9ad;
+      border-color: #a6a9ad;
+      color: #fff;
+    }
+    &:active {
+      background: #82848a;
+      border-color: #82848a;
+      color: #fff;
+    }
+  }
+  &--warning {
+    color: #fff;
+    background-color: #e6a23c;
+    border-color: #e6a23c;
+    &:hover,
+    &:focus {
+      background: #ebb563;
+      border-color: #ebb563;
+      color: #fff;
+    }
+    &:active {
+      background: #cf9236;
+      border-color: #cf9236;
+      color: #fff;
+    }
+  }
+  &--danger {
+    color: #fff;
+    background-color: #f56c6c;
+    border-color: #f56c6c;
+    &:hover,
+    &:focus {
+      background: #f78989;
+      border-color: #f78989;
+      color: #fff;
+    }
+    &:active {
+      background: #dd6161;
+      border-color: #dd6161;
+      color: #fff;
+    }
   }
 }
 </style>>
