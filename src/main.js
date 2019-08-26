9f6e76f6e5e893a96a76d9f836576e6964e58a19
wubh2012 <wubh2012@163.com>
2019-08-26 17:49:07 +0800

    完成第一个button 的测试用例

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import Button from "./components/Button.vue";
 import Icon from "./components/Icon.vue";
 import ButtonGroup from "./components/ButtonGroup.vue";
+import chai from 'chai'
+
 
 Vue.config.productionTip = false
 Vue.component(Button.name, Button)
@@ -12,3 +14,23 @@ Vue.component(ButtonGroup.name, ButtonGroup)
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
+
+
+const expect = chai.expect
+
+{
+  const Constructor = Vue.extend(Button)
+  const vm = new Constructor({
+    propsData: {
+      icon: 'w-icon-setting'
+    }
+  })
+  vm.$mount('#test')
+  let useElement = vm.$el.querySelector('use')
+  let href = useElement.getAttribute('xlink:href')
+  expect(href).to.eq('#w-icon-setting')
+  vm.$el.remove()
+  vm.$destroy()
+
+}
